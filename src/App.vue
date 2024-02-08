92d502e2496691bddd1a769b45c598d0d9d0189f
agnesvarga1 <agikolyok@gmail.com>
2024-02-08 20:59:44 +0100

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+import BasicComp from "./components/BasicComp.vue";
 export default {
   name: "App",
+  components: {
+    BasicComp,
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <BasicComp />
+</template>
 
 <style lang="scss">
 @use "./styles/general.scss";
+body {
+  background-color: #121212;
+  color: aliceblue;
+  text-align: center;
+  padding: 3rem;
+}
 </style>
--- src/components/BasicComp.vue
+++ src/components/BasicComp.vue
@@ -1,5 +1,7 @@
 <script>
 export default {};
 </script>
-<template></template>
+<template>
+  <h2>This is a basic component to test the template repo</h2>
+</template>
 <style lang="scss" scoped></style>
